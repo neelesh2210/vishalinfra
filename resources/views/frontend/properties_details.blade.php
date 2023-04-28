43d3a37cae80b8b00dc602c7f164f738f58046d3
markshailesh <shaileshhq@gmail.com>
2023-04-28 16:27:50 +0530

    updte details page

--- resources/views/frontend/properties_details.blade.php
+++ resources/views/frontend/properties_details.blade.php
@@ -85,179 +85,23 @@
 										<li class="col-lg-4 col-md-6 mb-2 p-0"><i class="fa fa-spa mr-1"></i>Free Spa</li>
 									</ul>
 								</div>
-
-							</div>
-
-							<!-- Single Block Wrap -->
-							<div class="property_block_wrap">
-
-								<div class="property_block_wrap_header">
-									<h4 class="property_block_title">Ameneties</h4>
-								</div>
-
-								<div class="block-body">
-									<ul class="avl-features third">
-										<li class="active">Air Conditioning</li>
-										<li class="active">Swimming Pool</li>
-										<li class="active">Central Heating</li>
-										<li class="active">Laundry Room</li>
-										<li class="active">Gym</li>
-										<li class="active">Alarm</li>
-										<li class="active">Window Covering</li>
-										<li class="active">Internet</li>
-										<li class="active">Pets Allow</li>
-										<li class="active">Free WiFi</li>
-										<li>Car Parking</li>
-										<li>Spa & Massage</li>
-									</ul>
-								</div>
 							</div>
 						</div>
 
 						<!-- property Sidebar -->
 						<div class="col-lg-4 col-md-12 col-sm-12">
 							<div class="property-sidebar">
-								<div class="sider_blocks_wrap">
-									<div class="side-booking-header">
-										<div class="sb-header-left"><h3 class="price">₹470<sub>/Night</sub><span class="offs">₹510</span></h3></div>
-										<div class="price_offer">20% Off</div>
-									</div>
-									<div class="side-booking-body">
-										<div class="row">
-											<div class="col-lg-6 col-md-6 col-sm-6 col-6">
-												<div class="form-group">
-													<label>Check In</label>
-													<div class="cld-box">
-														<i class="ti-calendar"></i>
-														<input type="text" name="checkin" class="form-control" value="10/24/2020" />
-													</div>
-												</div>
-											</div>
-											<div class="col-lg-6 col-md-6 col-sm-6 col-6">
-												<div class="form-group">
-													<label>Check Out</label>
-													<div class="cld-box">
-														<i class="ti-calendar"></i>
-														<input type="text" name="checkout" class="form-control" value="10/24/2020" />
-													</div>
-												</div>
-											</div>
-											<div class="col-lg-6 col-md-6 col-sm-6 col-6">
-												<div class="form-group">
-													<div>
-													  <label for="guests">Adults</label>
-													  <div class="guests-box">
-														  <button class="counter-btn" type="button" id="cnt-down"><i class="ti-minus"></i></button>
-														  <input type="text" id="guestNo" name="guests" value="2"/>
-														  <button class="counter-btn" type="button" id="cnt-up"><i class="ti-plus"></i></button>
-													  </div>
-													</div>
-												</div>
-											</div>
-											<div class="col-lg-6 col-md-6 col-sm-6 col-6">
-												<div class="form-group">
-													<div class="guests">
-													  <label>Kids</label>
-													  <div class="guests-box">
-														  <button class="counter-btn" type="button" id="kcnt-down"><i class="ti-minus"></i></button>
-														  <input type="text" id="kidsNo" name="kids" value="0"/>
-														  <button class="counter-btn" type="button" id="kcnt-up"><i class="ti-plus"></i></button>
-													  </div>
-													</div>
-												</div>
-											</div>
-
-											<div class="col-lg12 col-md-12 col-sm-12 mt-3">
-												<label for="guests">Advance features</label>
-												<div class="_adv_features_list">
-													<ul class="no-ul-list">
-														<li>
-															<input id="a-1" class="checkbox-custom" name="a-1" type="checkbox">
-															<label for="a-1" class="checkbox-custom-label">Air Condition<i>₹10</i></label>
-														</li>
-														<li>
-															<input id="a-2" class="checkbox-custom" name="a-2" type="checkbox" checked>
-															<label for="a-2" class="checkbox-custom-label">Bedding<i>₹07</i></label>
-														</li>
-														<li>
-															<input id="a-3" class="checkbox-custom" name="a-3" type="checkbox" checked>
-															<label for="a-3" class="checkbox-custom-label">Heating<i>₹20</i></label>
-														</li>
-														<li>
-															<input id="a-4" class="checkbox-custom" name="a-4" type="checkbox">
-															<label for="a-4" class="checkbox-custom-label">Internet<i>₹10</i></label>
-														</li>
-														<li>
-															<input id="a-5" class="checkbox-custom" name="a-5" type="checkbox">
-															<label for="a-5" class="checkbox-custom-label">Microwave<i>₹05</i></label>
-														</li>
-													</ul>
-												</div>
-											</div>
-
-											<div class="col-lg12 col-md-12 col-sm-12 mt-3">
-												<label for="guests">Price & Tax</label>
-												<div class="_adv_features">
-													<ul>
-														<li>I Night<span>₹310</span></li>
-														<li>Discount 25₹<span>-₹250</span></li>
-														<li>Service Fee<span>₹17</span></li>
-														<li>Breakfast Per Adult<span>₹35</span></li>
-													</ul>
-												</div>
-											</div>
-
-											<div class="side-booking-foot">
-												<span class="sb-header-left">Total Payment</span>
-												<h3 class="price theme-cl">₹170</h3>
-											</div>
-
-											<div class="col-lg-12 col-md-12 col-sm-12">
-												<div class="stbooking-footer mt-1">
-													<div class="form-group mb-0 pb-0">
-														<a href="#" class="btn book_btn theme-bg">Book It Now</a>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-
-								<!-- Agent Detail -->
-								<div class="sider_blocks_wrap">
-									<div class="side-booking-body">
-										<div class="agent-_blocks_title">
-
-											<div class="agent-_blocks_thumb"><img src="{{ asset('frontend/assets/img/user-6.jpg')}}" alt=""></div>
-											<div class="agent-_blocks_caption">
-												<h4><a href="#">Shivangi Preet</a></h4>
-												<span class="approved-agent"><i class="ti-check"></i>approved</span>
-											</div>
-											<div class="clearfix"></div>
-										</div>
-
-										<a href="#" class="agent-btn-contact" data-toggle="modal" data-target="#autho-message"><i class="ti-comment-alt"></i>Message</a>
-
-										<span id="number" data-last="+1234567896">
-											<span><i class="ti-headphone-alt"></i><a class="see">+355(44)35...Show</a></span>
-										</span>
-									</div>
-								</div>
-
 								<!-- Similar Property -->
 								<div class="sidebar-widgets">
-
 									<h4>Similar Property</h4>
-
 									<div class="sidebar_featured_property">
-
 										<!-- List Sibar Property -->
 										<div class="sides_list_property">
 											<div class="sides_list_property_thumb">
 												<img src="{{ asset('frontend/assets/img/p-1.png')}}" class="img-fluid" alt="" />
 											</div>
 											<div class="sides_list_property_detail">
-												<h4><a href="single-property-1.html">Loss vengel New Apartment</a></h4>
+												<h4><a href="#">Loss vengel New Apartment</a></h4>
 												<span><i class="ti-location-pin"></i>Sans Fransico</span>
 												<div class="lists_property_price">
 													<div class="lists_property_types">
@@ -276,7 +120,7 @@
 												<img src="{{ asset('frontend/assets/img/p-4.png')}}" class="img-fluid" alt="" />
 											</div>
 											<div class="sides_list_property_detail">
-												<h4><a href="single-property-1.html">Montreal Quriqe Apartment</a></h4>
+												<h4><a href="#">Montreal Quriqe Apartment</a></h4>
 												<span><i class="ti-location-pin"></i>Liverpool, London</span>
 												<div class="lists_property_price">
 													<div class="lists_property_types">
@@ -295,7 +139,7 @@
 												<img src="{{ asset('frontend/assets/img/p-7.png')}}" class="img-fluid" alt="" />
 											</div>
 											<div class="sides_list_property_detail">
-												<h4><a href="single-property-1.html">Curmic Studio For Office</a></h4>
+												<h4><a href="#">Curmic Studio For Office</a></h4>
 												<span><i class="ti-location-pin"></i>Montreal, Canada</span>
 												<div class="lists_property_price">
 													<div class="lists_property_types">
@@ -314,7 +158,7 @@
 												<img src="{{ asset('frontend/assets/img/p-5.png')}}" class="img-fluid" alt="" />
 											</div>
 											<div class="sides_list_property_detail">
-												<h4><a href="single-property-1.html">Montreal Quebec City</a></h4>
+												<h4><a href="#">Montreal Quebec City</a></h4>
 												<span><i class="ti-location-pin"></i>Sreek View, New York</span>
 												<div class="lists_property_price">
 													<div class="lists_property_types">
@@ -326,14 +170,10 @@
 												</div>
 											</div>
 										</div>
-
 									</div>
-
 								</div>
-
 							</div>
 						</div>
-
 					</div>
 				</div>
 			</section>
