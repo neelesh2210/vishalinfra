2b5e935588a8d1e00cb68489d4bf89466f864903
markshailesh <shaileshhq@gmail.com>
2023-05-29 14:03:02 +0530

    updtr style

--- resources/views/frontend/property_listing.blade.php
+++ resources/views/frontend/property_listing.blade.php
@@ -32,7 +32,7 @@
         float: left;
         color: #606060;
         border-left: 1px solid #e0e0e0;
-        padding: 10px 20px;
+        padding: 10px 18px;
         cursor: pointer;
         list-style: none;
         text-align: center;
