0b911e2da31c1bfdd58263b3323d8df1f54522d9
shaileshhq@gmail.com <shaileshhq@gmail.com>
2023-10-28 12:01:05 +0530

    prper

--- resources/views/frontend/properties.blade.php
+++ resources/views/frontend/properties.blade.php
@@ -92,8 +92,10 @@
                             </div>
                         </div>
                     </div>
-                    {{-- <img src="{{asset('backend/img/banners/'.$city_banner)}}" onerror="this.onerror=null;this.src='{{ asset('frontend/assets/img/bg.jpg') }}'" > --}}
                     <div class="row justify-content-center">
+                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-3">
+                        <img src="{{asset('backend/img/banners/'.$city_banner)}}" onerror="this.onerror=null;this.src='{{ asset('frontend/assets/img/bg.jpg') }}'" class="img-fluid">
+                        </div>
                         @foreach($properties as $property)
                             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-3">
                                 <div class="property-listing list_view row m-0">
